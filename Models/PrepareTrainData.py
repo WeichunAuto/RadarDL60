c0062e33e0c257356b4a375db7141e79bb99b31b
BobbyAuto <seven.wangweichun@outlook.com>
2024-04-10 12:29:27 +1200

    Prepare dataloader for cross train and validation.

--- Models/PrepareTrainData.py
+++ Models/PrepareTrainData.py
@@ -6,6 +6,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 from pathlib import Path
+import re
 
 
 class RadarDataset(Dataset):
@@ -21,9 +22,9 @@ class RadarDataset(Dataset):
 
 
 class PrepareTrainData:
-    def __init__(self, window_size=5, fs=2000, batch_size=16, is_shuffle=False):
-        self.window_size = window_size
-        self.fs = fs
+    def __init__(self, seq_length=5, n_features=118, batch_size=16, is_shuffle=False):
+        self.seq_length = seq_length
+        self.n_features = n_features
         self.batch_size = batch_size
         self.is_shuffle = is_shuffle
 
@@ -41,7 +42,7 @@ class PrepareTrainData:
             df_dataset = pd.concat([df_dataset, df], ignore_index=True)
             print(f'file_name-{i} = {file_path}')
 
-            if i > 1:
+            if i > 3:
                 break
 
         last_column = "f_590"
@@ -49,6 +50,7 @@ class PrepareTrainData:
 
         X_data = df_X.to_numpy()
         y_data = df_dataset["hr"].to_numpy()
+        print(f'x_data length = {X_data.shape}')
 
         return X_data, y_data
 
@@ -79,7 +81,7 @@ class PrepareTrainData:
         return self.get_dataloader(X_test, y_test)
 
     def get_dataloader(self, X_data, y_data):
-        X_data = X_data.reshape(len(X_data), self.window_size, 118)
+        X_data = X_data.reshape(len(X_data), self.seq_length, self.n_features)
         y_data = y_data.reshape(len(y_data), 1)
         X_data = torch.tensor(X_data).float()
         y_data = torch.tensor(y_data).float()
@@ -87,6 +89,54 @@ class PrepareTrainData:
         dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=self.is_shuffle)
         return dataloader
 
+    def get_cross_dataloaders(self, participant_id):
+        '''
+        Prepare dataloader for cross validation.
+        :param participant_id:
+        :return:
+        '''
+        parent_dir = str(Path.cwd().parent)
+        dataset_directory = parent_dir + "/publicdata/dataset/cross_train/"
+        val_file_name = "raw_" + str(participant_id) + "_Resting.csv"
+        df_dataset = None
+
+        # prepare train dataloader
+        file_names = [file_name for file_name in os.listdir(dataset_directory) if file_name.startswith("raw_") and file_name != val_file_name]
+        file_names = sorted(file_names, key=lambda x: int(re.findall(r'\d+', x)[0]))
+        for i, file_name in enumerate(file_names):
+            file_path = os.path.join(dataset_directory, file_name)
+            # print(f'training file_name-{i} = {file_path}')
+            df = pd.read_csv(file_path)
+            df_dataset = pd.concat([df_dataset, df], ignore_index=True)
+
+        last_column = "f_590"
+        df_train_X = df_dataset.loc[:, "f_1":last_column]
+
+        Train_X_data = df_train_X.to_numpy()
+        Train_y_data = df_dataset["hr"].to_numpy()
+        # print(f'Train_X_data shape = {Train_X_data.shape}, Train_y_data shape = {Train_y_data.shape}')
+        train_X = Train_X_data.reshape(len(Train_X_data), self.seq_length, self.n_features)
+        train_y = Train_y_data.reshape(len(Train_y_data), 1)
+        train_X = torch.tensor(train_X).float()
+        train_y = torch.tensor(train_y).float()
+        train_dataset = RadarDataset(train_X, train_y)
+        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=self.is_shuffle)
+
+        # prepare validation train dataloader
+        df_val = pd.read_csv(os.path.join(dataset_directory, val_file_name))
+
+        df_val_X = df_val.loc[:, "f_1":last_column]
+        val_X_data = df_val_X.to_numpy()
+        val_y_data = df_val["hr"].to_numpy()
+        # print(f'val_X_data shape = {val_X_data.shape}, val_y_data shape = {val_y_data.shape}')
+        val_X = val_X_data.reshape(len(val_X_data), self.seq_length, self.n_features)
+        val_y = val_y_data.reshape(len(val_y_data), 1)
+        val_X = torch.tensor(val_X).float()
+        val_y = torch.tensor(val_y).float()
+        val_dataset = RadarDataset(val_X, val_y)
+        val_dataloader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=self.is_shuffle)
+
+        return train_dataloader, val_dataloader
 
 
-   # X_data, y_data = PrepareTrainData(is_shuffle=False).get_data(show=True, isEval=False, is_complex=True)
+# PrepareTrainData().get_cross_dataloaders(26)
