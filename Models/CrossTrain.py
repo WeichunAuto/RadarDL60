55db17632a27710d3364629aa97ea4e0b1baf0e1
BobbyAuto <seven.wangweichun@outlook.com>
2024-04-22 00:00:22 +1200

    modify start_training to support cross train for a participant.

--- Models/CrossTrain.py
+++ Models/CrossTrain.py
@@ -5,6 +5,9 @@ import numpy as np
 import pandas as pd
 
 import sys
+
+
+
 sys.path.append('/home/syt0722/Weichun/60pts')
 from Models.ExecuteTrainModels import ExecuteTrainModels
 from Models.ModelNames import ModelNames
@@ -12,7 +15,8 @@ from Models.LSTM.RadarLSTM import RadarLSTM
 from Models.BiLSTM.RadarBiLSTM import RadarBiLSTM
 from Models.GRU.RadarGRU import RadarGRU
 from Models.TPALSTM.RadarTpaLSTM import RadarTpaLSTM
-from Models.NBEATS.NBeats import NBeats
+# from Models.NBEATS.NBeats import NBeats
+from Models.DILATE.NetGRU import NetGRU
 
 
 def get_model(model_name):
@@ -23,22 +27,28 @@ def get_model(model_name):
         model = RadarBiLSTM()
     elif model_name == ModelNames.GRU.value:
         model = RadarGRU()
+    elif model_name == ModelNames.DILATE.value:
+        model = NetGRU()
     elif model_name == ModelNames.TPALSTM.value:
         model = RadarTpaLSTM()
-    elif model_name == ModelNames.NBEATS.value:
-        model = NBeats()
+    # elif model_name == ModelNames.NBEATS.value:
+    #     model = NBeats()
+
 
     return model
 
 
-def start_training(model_name, epochs):
+def start_training(model_name, epochs, participant=-1):
     pd_columns = ["t_loss", "v_loss", "p_id"]
     pd_values = None
     current_dir = str(Path.cwd())
     save_path = os.path.join(current_dir, model_name, "trained_models")
     saved_file_path = os.path.join(save_path, "loss_" + model_name + ".csv")
     # all participants ID
-    participant_ids = [i for i in range(1, 31) if i != 3]
+    if participant == -1:
+        participant_ids = [i for i in range(1, 31) if i != 3]
+    else:
+        participant_ids = [participant]
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     for participant_id in participant_ids:
@@ -70,5 +80,8 @@ epochs = 50
 # model_name = ModelNames.TSFM.value
 # start_training(model_name, epochs)
 
-model_name = ModelNames.NBEATS.value
+# model_name = ModelNames.NBEATS.value
+# start_training(model_name, epochs)
+
+model_name = ModelNames.DILATE.value
 start_training(model_name, epochs)
