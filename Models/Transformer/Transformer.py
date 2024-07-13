533b29af737dcfa6d8c09f0c7cd2796c75eb0445
WeichunAuto <seven.wangweichun@outlook.com>
2024-07-13 18:43:12 +1200

    Transforer

---
+++ Models/Transformer/Transformer.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        # div_term = torch.exp(
+        #     torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        # )
+        div_term = 1 / (10000 ** ((2 * np.arange(d_model)) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term[0::2])
+        pe[:, 1::2] = torch.cos(position * div_term[1::2])
+
+        pe = pe.unsqueeze(0).transpose(0, 1)  # [5000, 1, d_model],so need seq-len <= 5000
+        # pe.requires_grad = False
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # print(self.pe[:x.size(0), :].repeat(1,x.shape[1],1).shape ,'---',x.shape)
+        # dimension 1 maybe inequal batchsize
+        return x + self.pe[:x.size(0), :].repeat(1, x.shape[1], 1)
+
+
+class Transformer(nn.Module):
+    def __init__(self, feature_size=1080, num_layers=1, dropout=0.1):
+        super(Transformer, self).__init__()
+
+        self.lr = 0.001
+        self.loss_fun = nn.MSELoss()
+
+        self.model_type = 'Transformer'
+        self.input_embedding = nn.Linear(590, feature_size)
+        self.src_mask = None
+
+        self.pos_encoder = PositionalEncoding(feature_size)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=feature_size, nhead=10, dropout=dropout)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
+        self.decoder = nn.Linear(feature_size, 1)
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src):
+        # src with shape (input_window, batch_len, 1)
+        src = src.view(1, src.size(0), 590)
+        if self.src_mask is None or self.src_mask.size(0) != len(src):
+            device = src.device
+            mask = self._generate_square_subsequent_mask(len(src)).to(device)
+            self.src_mask = mask
+
+        src = self.input_embedding(src)  # linear transformation before positional embedding
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, self.src_mask)  # , self.src_mask)
+        output = self.decoder(output)
+        output = output.squeeze(0)
+        return output
+
+    def _generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
